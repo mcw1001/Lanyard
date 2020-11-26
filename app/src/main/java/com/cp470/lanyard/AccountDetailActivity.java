e821a1c1cda1b27150a179f2c59280efeeb4cd39
MorgenneB <morgenne.b@gmail.com>
2020-11-26 14:40:51 -0500

    Improvements to Detail view styling. Detail view now pulls real data. Merged edit functionality into Detail view

--- app/src/main/java/com/cp470/lanyard/AccountDetailActivity.java
+++ app/src/main/java/com/cp470/lanyard/AccountDetailActivity.java
@@ -1,13 +1,41 @@
 package com.cp470.lanyard;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.accounts.Account;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.Timestamp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class AccountDetailActivity extends AppCompatActivity {
+    protected TextView accountTitle;
+    protected ImageView accountIcon;
+    protected TextView accountUsername;
+    protected TextView accountPassword;
+    protected TextView accountCreation;
+    protected TextView accountNotes;
+    protected ProgressBar loadingBar;
+    protected ConstraintLayout accountDetails;
+    protected ImageView togglePassword;
 
     // Firebase stuff
     private FirebaseAuth mAuth;
@@ -19,25 +47,15 @@ public class AccountDetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         Intent intent = getIntent();
         documentId = intent.getExtras().getString("documentId");
-        AccountItem account = new AccountItem(
-                R.drawable.ic_baseline_mail_24,
-                "Fake Mail",
-                "jeffBob@fake.com",
-                "fyt6fygt7ygy76yg"
-        );
         setContentView(R.layout.activity_account_detail);
-        // TODO replace mock data
-        TextView accountTitle = findViewById(R.id.detailTitleAccountItem);
-        accountTitle.setText(account.getTitle());
-        ImageView accountIcon = findViewById(R.id.detailIconAccountItem);
-        accountIcon.setImageResource(account.getImageResource());
-        TextView accountUsername = findViewById(R.id.detailUserNameAccountItem);
-        accountUsername.setText(account.getUserName());
-        TextView accountPassword = findViewById(R.id.detailPassAccountItem);
-        accountPassword.setText(account.getPassword());
-        TextView accountCreation = findViewById(R.id.detailCreationDate);
-        accountCreation.setText("April 2, 2020");
-        TextView accountNotes = findViewById(R.id.detailPassNotes);
+        accountTitle = findViewById(R.id.detailTitleAccountItem);
+        accountIcon = findViewById(R.id.detailIconAccountItem);
+        accountUsername = findViewById(R.id.detailUserNameAccountItem);
+        accountPassword = findViewById(R.id.detailPassAccountItem);
+        accountPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
+        accountCreation = findViewById(R.id.detailCreationDate);
+        accountNotes = findViewById(R.id.detailPassNotes);
+        togglePassword = findViewById(R.id.detail_toggle_password);
         accountNotes.setText("Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
                 "Phasellus sem quam, maximus eu vehicula id, sagittis quis sem. In luctus ultricies lacus nec lobortis. Aliquam sed porttitor mauris. " +
                 "Etiam sit amet imperdiet ante. Morbi suscipit ultrices ante, ut euismod massa rhoncus et. " +
@@ -46,6 +64,8 @@ public class AccountDetailActivity extends AppCompatActivity {
                 "Aenean est sapien, commodo vel est accumsan, tincidunt pellentesque diam. " +
                 "Praesent libero quam, vestibulum nec efficitur in, lacinia ut nunc. Suspendisse malesuada euismod nunc vitae euismod. "
         );
+
+        loadAccountItem();
     }
 
     private void loadAccountItem() {
@@ -64,11 +84,27 @@ public class AccountDetailActivity extends AppCompatActivity {
                         AccountItem accountItem = documentSnapshot.toObject(AccountItem.class);
 
                         accountTitle.setText(accountItem.getTitle());
-                        // accountIcon.
+                        // accountIcon.setImageResource(accountItem.getImageResource());
+                        // TODO swap out for actual icon
+                        accountIcon.setImageResource(R.drawable.placeholder);
                         accountUsername.setText(accountItem.getUserName());
                         accountPassword.setText(accountItem.getPassword());
+                        Date creationDate = accountItem.getTimestamp().toDate();
+                        SimpleDateFormat format = new SimpleDateFormat("MMMM dd, YYYY");
+                        accountCreation.setText(format.format(creationDate));
+                        // TODO remove me I am just for testing
+                        try {
+                            Thread.sleep(1000);
+                        } catch(Exception e){
+
+                        }
+                        loadingBar = findViewById(R.id.detail_loading);
+                        accountDetails = findViewById(R.id.account_details);
+                        loadingBar.setVisibility(View.INVISIBLE);
+                        accountDetails.setVisibility(View.VISIBLE);
+
                     } else {
-                        Toast.makeText(AccountListItemDetailView.this, "Error loading password", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(AccountDetailActivity.this, "Error loading password", Toast.LENGTH_SHORT).show();
                     }
                 }
             })
@@ -80,7 +116,31 @@ public class AccountDetailActivity extends AppCompatActivity {
             });
     }
 
+    // Toggles password visibility
+    public void onTogglePasswordView(View view) {
+        if (accountPassword.getTransformationMethod() == PasswordTransformationMethod.getInstance()) {
+            accountPassword.setTransformationMethod(null);
+            togglePassword.setImageResource(R.drawable.ic_baseline_visibility_off_24);
+        } else {
+            accountPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
+            togglePassword.setImageResource(R.drawable.ic_baseline_visibility_24);
+        }
+    }
+
     public void onEditAccountClick(View view) {
-        // TODO Toggle account edit
+        Intent i = new Intent(this, AccountListItemDetailView.class);
+        i.putExtra("documentId", documentId);
+        startActivityForResult(i, 15);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // Account details changed successfully; reload with new data
+        if (resultCode == 1) {
+            loadingBar.setVisibility(View.VISIBLE);
+            accountDetails.setVisibility(View.INVISIBLE);
+            loadAccountItem();
+        }
     }
 }
